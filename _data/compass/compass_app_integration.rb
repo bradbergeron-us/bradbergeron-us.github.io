9a2758127ab84e4a97e45cc157218f3bc514e20e
Bradley Bergeron <bergeron.bradley@gmail.com>
2015-05-22 09:50:59 -0400

    Add Gem Dempendencies
    configure Compass, sitemap generator and auto-link email settings

---
+++ _data/compass/compass_app_integration.rb
@@ -0,0 +1,30 @@
+require 'compass/app_integration'
+require 'jekyll/compass/compass_installer'
+
+module Jekyll
+  module Compass
+    # Integration class to manage a new project type in Compass
+    class CompassAppIntegration
+      def self.installer(*args)
+        CompassInstaller.new(*args)
+      end
+
+      def self.configuration
+        project_path = ::Compass.configuration.project_path
+        config_file = File.join(project_path, '_data/compass.yml')
+        if File.exist? config_file
+          config = CompassConfiguration.from_yaml_file config_file
+          config[:css_dir] = "_site/#{config[:css_dir]}"
+        else
+          config = CompassConfiguration.default_configuration
+        end
+        config[:config_file] = config_file
+
+        config.to_compass
+      end
+    end
+  end
+end
+
+# Register our project type with compass
+::Compass::AppIntegration.register(:jekyll, 'Jekyll::Compass::CompassAppIntegration')
